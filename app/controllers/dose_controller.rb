cf7cc7ca324062499b016463daf0d6aad520a54e
Olivier Mathey <omathey@skynet.be>
2019-11-14 14:04:55 +0100

    rspec des validates et dbok

---
+++ app/controllers/dose_controller.rb
@@ -0,0 +1,22 @@
+class DoseController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
---
+++ app/controllers/ingredient_controller.rb
@@ -0,0 +1,22 @@
+class IngredientController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
